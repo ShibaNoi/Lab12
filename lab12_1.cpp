66966b375850c6e68a22d26c22dfcf2471cb3926
ShibaNoi <evergreen19832@gmail.com>
2026-01-06 22:13:58 +0700

    lab12_1.cpp completed

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -1,9 +1,17 @@
 #include<iostream>
 using namespace std;
+long long fibonacci(long long x);
 
 int main(){
     int x;
     cin >> x;
     cout << fibonacci(x);
     return 0; 
+}
+long long fibonacci(long long x){
+    if(x<=1){
+        return x;
+    }else{
+        return fibonacci(x-1) + fibonacci(x-2);
+    }
 }
